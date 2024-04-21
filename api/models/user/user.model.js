df01649c64de30a4bf917b02ce834cdd23bf3f59
Kilyong Jeong <uzoolove@gmail.com>
2024-04-21 09:42:36 +0900

    ✨ 지정한 사용자의 게시글 작성수, 작성한 게시글의 조회수, 북마크 수 조회 기능

--- api/models/user/user.model.js
+++ api/models/user/user.model.js
@@ -48,9 +48,129 @@ class UserModel{
   // 회원 정보 조회(모든 속성)
   async findById(_id){
     logger.trace(arguments);
-    const item = await this.db.user.findOne({ _id }, { projection: { password: 0, refreshToken: 0, }});
-    logger.debug(item);
-    return item;
+    
+    const pipeline = [
+      // Match stage to filter documents based on query
+      { $match: { _id } },
+
+      // 게시글 목록
+      {
+        $lookup: {
+          from: "post",
+          localField: "_id",
+          foreignField: "user._id",
+          as: "postItems"
+        }
+      },
+
+      // 게시글 수
+      {
+        $addFields: {
+          posts: { $size: "$postItems" }
+        }
+      },
+
+      // 북마크 목록
+      {
+        $lookup: {
+          from: "bookmark",
+          let: { userId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ["$user_id", "$$userId"] },
+                    { $eq: ["$type", "product"] } // 상품에 대한 북마크는 type이 product로 지정됨
+                  ]
+                }
+              }
+            }
+          ],
+          as: "bookmark.productItems"
+        }
+      },
+
+      {
+        $lookup: {
+          from: "bookmark",
+          let: { userId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ["$user_id", "$$userId"] },
+                    { $eq: ["$type", "user"] } // 상품에 대한 북마크는 type이 product로 지정됨
+                  ]
+                }
+              }
+            }
+          ],
+          as: "bookmark.userItems"
+        }
+      },
+
+      {
+        $lookup: {
+          from: "bookmark",
+          let: { userId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ["$user_id", "$$userId"] },
+                    { $eq: ["$type", "post"] } // 상품에 대한 북마크는 type이 product로 지정됨
+                  ]
+                }
+              }
+            }
+          ],
+          as: "bookmark.postItems"
+        }
+      },
+
+      // 북마크 수
+      {
+        $addFields: {
+          'bookmark.products': { $size: "$bookmark.productItems" },
+          'bookmark.users': { $size: "$bookmark.userItems" },
+          'bookmark.posts': { $size: "$bookmark.postItems" },
+        }
+      },
+
+      // 게시글 전체 조회수
+      { $unwind: "$postItems" },
+      {
+        $group: {
+          _id: null,
+          postViews: { $sum: "$postItems.views" },
+          user: { $first: "$$ROOT" }
+        }
+      },
+
+      { 
+        $project: { 
+          'user.password': 0,
+          'user.refreshToken': 0,
+          'user.postItems': 0,
+          'user.bookmark.productItems': 0,
+          'user.bookmark.userItems': 0,
+          'user.bookmark.postItems': 0,
+        }
+      },
+      
+    ];
+
+    const item = await this.db.user.aggregate(pipeline).next();
+
+    
+    // const item = await this.db.user.findOne({ _id }, { projection: { password: 0, refreshToken: 0, }});
+
+    const user = { ...item.user, postViews: item.postViews }
+    logger.debug(user);
+    return user;
   }
 
   // 회원 정보 수정
